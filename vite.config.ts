3b28e092a87979b15f6fd72dc25f4a68fbd19891
PhamHungHH <165739563+PhamHungHH@users.noreply.github.com>
2025-05-20 03:21:12 +0300

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,17 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/finland-tourist-website/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  }
 })
